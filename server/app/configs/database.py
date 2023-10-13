d1443e438468cdb1491f2cf86f6f5c43c1d19299
devlopersabbir <devlopersabbir@gmail.com>
2023-10-14 05:29:41 +0600

    📌 Fixed Installing issue 🐸

--- server/app/configs/database.py
+++ server/app/configs/database.py
@@ -2,17 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv, find_dotenv
-import sqlalchemy.orm
 load_dotenv(find_dotenv())
 
 
-DB_USERNAME = os.environ.get("DB_USERNAME")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-
-DABABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DABABASE_URL = os.environ.get("DABABASE_URL")
 
 
 engine = create_engine(DABABASE_URL)

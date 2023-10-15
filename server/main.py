a9c655b3bdb329f1727dd45493923fd955a93350
devlopersabbir <devlopersabbir@gmail.com>
2023-10-16 03:01:27 +0600

    🤝 Product routes add on main file 👨🏼

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from app.routes import userRoutes
+from app.routes import userRoutes, productRoutes
 from app.configs.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
@@ -8,6 +8,7 @@ app = FastAPI()
 
 
 app.include_router(userRoutes.router, prefix="/api/v1/users")
+app.include_router(productRoutes.router, prefix="/api/v1/products")
 
 
 @app.get("/", tags=["Checker"])
